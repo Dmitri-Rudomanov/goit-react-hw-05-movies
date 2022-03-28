505701f0c061355b5178afe04a83729e0159ea32
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-03-28 14:35:01 +0200

    fixed-mistakes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,25 @@
 import { lazy, Suspense } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Container from './Container/Container.js';
 import AppBar from './AppBar/AppBar.js';
+
+const HomePage = lazy(() =>
+  import('../views/HomePage.js' /* webpackChunkName: "home-page" */)
+);
+const MoviesPage = lazy(() =>
+  import('../views/MoviesPage.js' /* webpackChunkName: "movies-page" */)
+);
 
 export default function App() {
   return (
     <Container>
       <AppBar />
 
-      <Suspense>
-        <Switch>
-          <Route path="/" exact></Route>
-          <Route path="/movies" exact></Route>
-        </Switch>
+      <Suspense fallback={<h1>Loading....</h1>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+        </Routes>
       </Suspense>
     </Container>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,10 +4,16 @@ import s from './Navigation.module.css';
 export default function Navigation() {
   return (
     <nav>
-      <NavLink exact to="/" className={s.link} activeClassName={s.activeLink}>
+      <NavLink
+        to="/"
+        className={navData => (navData.isActive ? s.activeLink : s.link)}
+      >
         Home
       </NavLink>
-      <NavLink to="/movies" className={s.link} activeClassName={s.activeLink}>
+      <NavLink
+        to="/movies"
+        className={navData => (navData.isActive ? s.activeLink : s.link)}
+      >
         Movies
       </NavLink>
     </nav>
