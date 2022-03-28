a73abdccbfabf513ed65736aa74a911a4c979ef0
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-03-28 10:17:15 +0200

    start-working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,19 @@
-import { useState, useEffect } from 'react';
-import Searchbar from './Searchbar/Searchbar.js';
-import ImageGallery from './ImageGallery/ImageGallery.js';
-import pixabayAPI from '../services/pixabayApi.js';
-import Button from './Button/Button.js';
-import Loader from './Loader/Loader.js';
-import Modal from './Modal/Modal.js';
-import Error from './Error/Error.js';
-import s from './App.module.css';
-import { ToastContainer } from 'react-toastify';
-import { toast } from 'react-toastify';
+import { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Container from './Container/Container.js';
+import AppBar from './AppBar/AppBar.js';
 
 export default function App() {
-  const [images, setImages] = useState([]);
-  const [query, setQuery] = useState('');
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(null);
-  const [showModal, setShowModal] = useState(null);
-  const [largeImg, setLargeImg] = useState('');
-  const [showMore, setShowMore] = useState(true);
-
-  const onBtnClick = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
-  useEffect(() => {
-    if (query === '') {
-      return;
-    }
-    onFetchImg();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query, page]);
-
-  const handleGalleryImg = fullImgUrl => {
-    setLargeImg(fullImgUrl);
-    setShowModal(true);
-  };
-
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
-
-  const onFetchImg = () => {
-    setIsLoading(true);
-    pixabayAPI.fetchGallery(query, page).then(({ hits, totalHits }) => {
-      setImages([...images, ...hits]);
-      let check = images.length;
-      check += hits.length;
-      setShowMore(true);
-      setIsLoading(false);
-      if (check >= totalHits && totalHits !== 0) {
-        toast.warning('Sorry,you`ve reach the end of search result');
-        setShowMore(false);
-      }
-    });
-  };
-
-  const queryChange = query => {
-    setQuery(query);
-    setPage(1);
-    setImages([]);
-  };
-
-  const showMoreCheck = images.length !== 0 && !isLoading && showMore;
-  const errorCheck = images.length === 0 && query !== '' && !isLoading;
   return (
-    <div>
-      <Searchbar onSubmit={queryChange} />
-      <div className={s.App}>
-        {showModal && <Modal largeImage={largeImg} onClose={toggleModal} />}
-        <ImageGallery Images={images} onImgClick={handleGalleryImg} />
-        {isLoading && <Loader />}
-        {showMoreCheck && <Button onClick={onBtnClick} />}
-        {errorCheck && (
-          <Error
-            message={`Sorry, There is no picture matching search query: ${query}`}
-          />
-        )}
-      </div>
-      <ToastContainer autoClose={2500} />
-    </div>
+    <Container>
+      <AppBar />
+
+      <Suspense>
+        <Switch>
+          <Route path="/" exact></Route>
+          <Route path="/movies" exact></Route>
+        </Switch>
+      </Suspense>
+    </Container>
   );
 }
